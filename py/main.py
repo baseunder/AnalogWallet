e32e9bf7a476a290ef62a3aa9a485f8b32cdd8d9
Maël Distel <36006364+baseunder@users.noreply.github.com>
2023-07-26 22:24:06 +0200

    continue protocol implementation

--- py/main.py
+++ py/main.py
@@ -1,24 +1,29 @@
 import serial
 import time
 import hashlib
+import sys
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter
 from serial.tools import list_ports
 from pw_check import password_check
 
 def main():
-    # List all available serial ports and let the user select one
-    ports = list_ports.comports()
-    port_names = [port.device for port in ports]
-    print("-".join([" " for _ in range(10)]))
-    print("\n".join(port_names))
-    print("-".join([" " for _ in range(10)]))
-    port_completer = WordCompleter(port_names)
-    port_name = prompt("Enter port name: ", completer=port_completer)
+    port_name = None
+    if len(sys.argv)>1:
+        port_name = sys.argv[1]
+    else:
+        # List all available serial ports and let the user select one
+        ports = list_ports.comports()
+        port_names = [port.device for port in ports]
+        print("-".join([" " for _ in range(10)]))
+        print("\n".join(port_names))
+        print("-".join([" " for _ in range(10)]))
+        port_completer = WordCompleter(port_names)
+        port_name = prompt("Enter port name: ", completer=port_completer)
 
     # Initialize serial connection
     ser = serial.Serial(port_name, 115200)  # adjust baud rate as necessary
-    commands = ['init', 'version', 'open', 'erase', 'sign', 'restore', 'test', 'rnd', 'rea']
+    commands = ['init', 'version', 'open', 'erase', 'sign', 'restore', 'test', 'rnd', 'rea', 'blink']
     print("-".join([" " for _ in range(10)]))
     print("\n".join(commands))
     print("-".join([" " for _ in range(10)]))
@@ -29,38 +34,64 @@ def main():
         if not (command in commands):
             print("command",command,"not found")
             continue
-
+        cmIndex = commands.index(command).to_bytes(1, "little")
         # If the command requires a second part (like a password or hash), send it
         if command in {"init", "open"}:
             password = prompt("Enter password: ", is_password=True)
             if not password_check(password):
                 continue
-            passwordbytes = hashlib.sha256(password.encode())
-            ser.write(commands.index(command))
+            passwordbytes = hashlib.sha256(password.encode()).digest()
+            ser.write(cmIndex)
             ser.write(passwordbytes)
-
-        if command in {"sign"}:
-            hash = prompt("Enter hash: ")
+            while not ser.in_waiting:
+                time.sleep(0.05)
+            fis = ser.read_all()
+            if len(fis)==64:
+                print(fis.hex())
+                while not ser.in_waiting:
+                    time.sleep(0.05)
+                fis = ser.read_all()
+                print(fis.hex())
+            elif len(fis)==1:
+                print(fis[0])
+        elif command in {"sign"}:
+            hash = prompt("Enter hash [HEX]: ")
             hashB = bytes.fromhex(hash)
-            if hashB.count() != 32:
+            if len(hashB) != 32:
                 print("hash not valid")
                 continue
-            ser.write(commands.index(command))
+            ser.write(cmIndex)
             ser.write(hashB)
-
-        # If the command is "restore", send the backup
+            while not ser.in_waiting:
+                time.sleep(0.05)
+            fis = ser.read_all()
+            if len(fis)==64:
+                print(fis.hex())
+            print("the wallet is now closed until you reconnect it")
         elif command == "restore":
             backup = prompt("Enter backup name (filename on the SD Card): ")
-            ser.write(commands.index(command))
+            ser.write(cmIndex)
             ser.write((backup + '\n').encode())
-
-        while not ser.in_waiting:
-            time.sleep(0.1)
-        # Read and print the response
-        while ser.in_waiting:
-            response = ser.readline().decode().strip()
-            print(f"Response: {response}")
-
+            while not ser.in_waiting:
+                time.sleep(0.05)
+            fis = ser.read()
+        elif command == "erase":
+            ser.write(cmIndex)
+            print(int.from_bytes(ser.read(),"little"))
+        elif command == "test":
+            ser.write(cmIndex)
+            while not ser.in_waiting:
+                time.sleep(0.05)
+            fis = ser.read_all()
+            if len(fis)==64:
+                print(fis.hex())
+            elif len(fis)==1:
+                print(fis[0])
+        elif command == "version":
+            ser.write(cmIndex)
+            print(ser.readline().decode())
+        else:
+            ser.write(cmIndex)
 if __name__ == "__main__":
     main()
 
