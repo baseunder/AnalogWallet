1347765b1bbdbd1cc6a0fd86068d25ab43f9e0db
Maël Distel <36006364+baseunder@users.noreply.github.com>
2023-11-22 08:59:51 +0100

    add small footprint SD card communication for the backup

--- bckp.cpp
+++ bckp.cpp
@@ -1,29 +1,27 @@
 #include "bckp.h"
 #include <EEPROM.h>
-#include "SDCARDmodded.h"
-//#include "RawSD.h"
-#include <SPI.h>
+#include "RawSD.h"
 
 uint8_t statb;
-int port = 4;
-unsigned long block = 1000;
 
+unsigned long block = 1000;
 unsigned char buffer[512];
-unsigned char error;
 bool performInit = true;
+RawSD mySD(10);
 uint8_t checkCard()
 {
   if (performInit){
     performInit = false;
-    pinMode(port, OUTPUT);
-    SPI.begin();
+    if (!mySD.begin()){
+      return 7;
+    }
   }
-  error = SDCARDmodded.readblock(block, port);
-  if (error)
+  if (mySD.readBlock(block, buffer))
   {
-    return error;
+    return 0;
+  }else{
+    return 7;
   }
-  return 0;
 }
 
 uint8_t initBackup(byte *p1, byte *pub1)
@@ -33,19 +31,17 @@ uint8_t initBackup(byte *p1, byte *pub1)
   memcpy(buffer, p1, 32);
   for (int i = 0; i < 16; i++)
   {
-    error = SDCARDmodded.writeblock(block*i, port);
-    if (error!=0)return 9;
+    if (!mySD.writeBlock(block*i, buffer))return 9;
   }
+  /*
   for (int i = 64+pub1[0]; i < 64+pub1[0]+16; i++)
   {
-    error = SDCARDmodded.writeblock(block*i, port);
-    if (error!=0)return 9;
+    if (!mySD.writeBlock(block*i, buffer))return 9;
   }
   for (int i = 400+pub1[1]; i < 400+pub1[1]+16; i++)
   {
-    error = SDCARDmodded.writeblock(block*i, port);
-    if (error!=0)return 9;
-  }
+    if (!mySD.writeBlock(block*i, buffer))return 9;
+  }*/
   return 0;
 }
 
@@ -65,7 +61,7 @@ uint8_t restoreBackup()
   statb = checkCard();
   if (statb) return statb;
   unsigned char *mm = (unsigned char*)buffer;
-  if (memcmp(mm, mm+1, 32)==0) {
+  if (memcmp(mm, mm+1, 31)==0) {
     return 13;
   }else{
     for (int i = 0; i < 32; i++)
